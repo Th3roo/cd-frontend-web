d9c51c80ef9a8aa7165fa111a952dd6d34a7cee1
verncat <183478378+verncat@users.noreply.github.com>
2025-12-05 16:40:02 +0400

    Window system. Remove QuickStartExample export from WindowSystem index

--- components/WindowSystem/index.ts
+++ components/WindowSystem/index.ts
@@ -2,6 +2,5 @@ export { WindowManagerProvider, useWindowManager } from "./WindowManager";
 export { default as WindowSystem } from "./WindowSystem";
 export { default as Window } from "./Window";
 export { default as Dock } from "./Dock";
-export { default as QuickStartExample } from "./examples/QuickStart";
 export * from "./types";
 export * from "./utils";
